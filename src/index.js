e5ac2dc1c9761698470814e2d4b0f05010fe523b
Marcon Willian <marcon@mh4sh.dev>
2021-08-28 18:58:53 -0300

    fix 🐞: Update base of api

--- src/index.js
+++ src/index.js
@@ -10,23 +10,75 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  
+  const user = users.find(user => user.username === username)
+
+  if(!user){
+    return response.status(404).json({error: 'User not exist'})
+  }
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const totalTodos = user.todos.length;
+
+  if(totalTodos >= 10 && !user.pro){
+    return response.status(403).json({error: 'Max todo permitted, requested pro plan.'})
+  }
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+  
+  const user = users.find(user => user.username === username)
+
+  if(!user){
+    return response.status(404).json({error: 'User not exist'})
+  }
+
+
+  if(!validate(id)) {
+    return response.status(400).json({error: "Invalid id"})
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+  
+  if(!todo) {
+    return response.status(404).json({error: "Todo not found"})
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  
+  const user = users.find(user => user.id === id)
+
+  if(!user){
+    return response.status(404).json({error: 'User not exist'})
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
+  
 
   const usernameAlreadyExists = users.some((user) => user.username === username);
 
